90e23faf3814b49cfbfe9695d93ed81b053c527f
viddi7 <vidar-oli@hotmail.com>
2019-05-06 15:26:33 +0000

    Sellers komið i gang

--- Castle/Castle/urls.py
+++ Castle/Castle/urls.py
@@ -22,5 +22,6 @@ from apartments import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="homepage"),
-    path("apartments/", include('apartments.urls'))
+    path("apartments/", include('apartments.urls')),
+    path("sellers/",include('sellers.urls'))
 ]
--- Castle/sellers/views.py
+++ Castle/sellers/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def sellers_list(request):
+    return render(request, "sellers/sellers_list.html")
