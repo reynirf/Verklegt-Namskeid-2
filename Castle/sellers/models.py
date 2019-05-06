d51fb6b2211f68fd6fad60e57d5a458cb0bf2c91
ithil13 <ithil13@gmail.com>
2019-05-06 17:57:00 +0000

    adding models for database

--- Castle/sellers/models.py
+++ Castle/sellers/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from users.models import User
+from users.models import Buyer
 
 class Seller(models.Model):
-    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=999)
     address = models.CharField(max_length=255)
     zip_code = models.IntegerField()
-    phone = models.IntegerField()
-    email = models.CharField(max_length=100)
+    logo = models.CharField(max_length=999)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Sales(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     apartment = models.ForeignKey('apartments.Apartment', on_delete=models.CASCADE)
-    # buyer = models.ForeignKey(Buyer)
+    buyer = models.ForeignKey(Buyer, on_delete=models.DO_NOTHING)
     date_sold = models.DateField()
     price = models.IntegerField()
--- Castle/users/models.py
+++ Castle/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=255)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=16)
+    security_question = models.CharField(max_length=255)
+    security_answer = models.CharField(max_length=255)
+
+class Buyer(models.Model):
+    profile_pic = models.CharField(max_length=999, blank=True)
+    credit_card = models.CharField(max_length=16, blank=True)
+    cc_expires = models.CharField(max_length=5, blank=True)
+    cc_cvc = models.IntegerField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
