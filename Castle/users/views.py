d915998a7109a58228d580ee8a32981279426527
viddi7 <vidar-oli@hotmail.com>
2019-05-07 00:45:17 +0000

    lagaði commu

--- Castle/users/views.py
+++ Castle/users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def login(request):
-    return render(request, "login_register.html")
+    return render(request, "users/login_register.html")
 
 def forgot_pass(request):
-    return render(request, "forgot_pass.html")
+    return render(request, "users/forgot_pass.html")
