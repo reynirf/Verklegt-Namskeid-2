09b9b0db77320499c4890b5fbbb1f4941cff0762
ithil13 <ithil13@gmail.com>
2019-05-08 18:33:16 +0000

    working on seller list and added seller info page

--- Castle/sellers/views.py
+++ Castle/sellers/views.py
@@ -6,3 +6,9 @@ from sellers.models import Seller
 def sellers_list(request):
     context = {'sellers': Seller.objects.all().order_by('address')}
     return render(request, "sellers/sellers_list.html", context)
+
+def seller_info(request, pk):
+    seller = Seller.objects.get(pk=pk)
+    return render(request, 'sellers/seller_info.html', {
+        'seller': seller
+    })
