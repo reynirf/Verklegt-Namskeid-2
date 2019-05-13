1d022eccc750dc17540d71141c033b2010af4087
Reynir <reynirfreyr9@gmail.com>
2019-05-13 17:40:27 +0000

    fix reseting passwords

--- Castle/users/urls.py
+++ Castle/users/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("login", LoginView.as_view(template_name='users/login.html'), name="login"),
     path("logout", LogoutView.as_view(next_page='homepage'), name="logout"),
-    path("forgot_pass", views.forgot_pass, name="forgot_pass"),
-    path("register_success", views.register_success, name="register_success"),
     path("profile", views.profile, name="profile"),
     path("edit_user", views.edit_user, name="edit_user"),
 
