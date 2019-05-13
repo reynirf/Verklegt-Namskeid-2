64ec57df8f580650d14fb0446c78e2fd6b8df72c
ithil13 <ithil13@gmail.com>
2019-05-13 20:57:43 +0000

    styling for contact info form

--- Castle/apartments/forms.py
+++ Castle/apartments/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class ContactInfoForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    ssn = forms.IntegerField()
-    country = forms.CharField()
-    address = forms.CharField(max_length=255)
-    zipcode = forms.IntegerField()
-    city = forms.CharField()
+    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    ssn = forms.IntegerField( widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'number'}), required=True)
+    country = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    address = forms.CharField(max_length=255,  widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    zipcode = forms.IntegerField( widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'number'}), required=True)
+    city = forms.CharField( widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
 
