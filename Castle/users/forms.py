3b3c483a42136afb2b86e48f6d60f31821797f42
Reynir <reynirfreyr9@gmail.com>
2019-05-13 15:32:28 +0000

    security question to registration form and style the form

--- Castle/users/forms.py
+++ Castle/users/forms.py
@@ -3,12 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
-    name = forms.CharField(max_length=255)
-    phone = forms.IntegerField()
-    email = forms.CharField(max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        super(NewUserForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+    name = forms.CharField(max_length=255, required=True)
+    phone = forms.IntegerField(required=True)
+    email = forms.CharField(max_length=100, required=True)
     seller = forms.BooleanField(required=False)
-    security_question = forms.CharField(max_length=255)
-    security_answer = forms.CharField(max_length=255)
+    CHOICES = (('What was your childhood nickname? ', 'What was your childhood nickname?'), ('In what city did you meet your spouse/significant other?', 'In what city did you meet your spouse/significant other?'), ("What is your oldest sibling's middle name?", "What is your oldest sibling's middle name?"), ("In what city or town was your first job?", 'In what city or town was your first job?'), ("What is the name of your favorite childhood friend?", 'What is the name of your favorite childhood friend?')
+               )
+    security_question = forms.ChoiceField(choices=CHOICES)
+    security_answer = forms.CharField(max_length=255, required=True)
     class Meta:
         model = User
         fields = ('username', 'name', 'phone', 'email', 'seller', 'password1', 'password2', 'security_question', 'security_answer',)
